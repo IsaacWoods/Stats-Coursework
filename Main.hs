3393380c44bdd680ce68920f02a7f6e90489f889
Isaac Woods <isaacwoods.home@gmail.com>
2017-03-12 21:46:17 +0000

    Test Spearman's rank stuff and things

--- Main.hs
+++ Main.hs
@@ -4,6 +4,7 @@
 
 import System.IO
 import Text.Read
+import Data.List
 import Graphics.Rendering.Chart.Easy
 import Graphics.Rendering.Chart.Backend.Cairo
 
@@ -34,7 +35,6 @@ extractFloat temp = case temp of Nothing -> 0.0
 -- Takes an average of the minimum and maximum temperatures and plots that against rainfall
 plotStation_MeanMinMax :: WeatherStation -> EC (Layout Float Float) ()
 plotStation_MeanMinMax station = do
---  let weatherData = parseData $ drop 7 $ lines weatherFile
   let juneMin     = [(extractFloat $ tmin m, extractFloat $ rainfall m) | m <- (weatherData station), month m == 6]
       julyMin     = [(extractFloat $ tmin m, extractFloat $ rainfall m) | m <- (weatherData station), month m == 7]
       augustMin   = [(extractFloat $ tmin m, extractFloat $ rainfall m) | m <- (weatherData station), month m == 8]
@@ -50,20 +50,39 @@ plotStation_MeanMinMax station = do
 -- Plots maximum temperature against rainfall
 plotStation_Max :: WeatherStation -> EC (Layout Float Float) ()
 plotStation_Max station = do
---  let weatherData = parseData $ drop 7 $ lines weatherFile
   let june        = [(extractFloat $ tmax m, extractFloat $ rainfall m) | m <- (weatherData station), month m == 6]
       july        = [(extractFloat $ tmax m, extractFloat $ rainfall m) | m <- (weatherData station), month m == 7]
       august      = [(extractFloat $ tmax m, extractFloat $ rainfall m) | m <- (weatherData station), month m == 8]
       mean        = zipWith3 (\a b c -> (((fst a) + (fst b) + (fst c)) / 3, ((snd a) + (snd b) + (snd c)) / 3)) june july august
   plot (points (name station) mean)
 
+qsort :: (Ord a) => [a] -> [a]
+qsort [] = []
+qsort (x:xs) = (qsort [l | l<-xs, l<=x])++[x]++(qsort [r | r<-xs, r>x])
+
+rank :: (Ord a, Fractional a) => [a] -> [a]
+rank xs = let n = fromIntegral $ length xs
+              xs' = qsort xs
+              rankerThingy x xs = let is = elemIndices x xs'
+                                  in (realToFrac (foldl (+) 0 is)) / (fromIntegral $ length is)
+          in map (\x -> n - ((rankerThingy x xs'))) xs
+
+-- NOTE(Isaac): there are assumed to be as many items of data in both sets
+spearmans :: (Ord a, Fractional a) => [a] -> [a] -> a
+spearmans xs ys = let n = fromIntegral $ length xs
+                  in 1.0 - (6.0 * (sum $ map (^^2) $ zipWith (-) (rank xs) (rank ys))) / (n * (n^^2 - 1.0))
+
 main :: IO ()
 main = do
-  weatherStations <- mapM (readFile . ("stations/"++)) ["armagh.txt", "heathrow.txt", "lowestoft.txt", "rossonwye.txt", "sheffield.txt", "nairn.txt", "eskdalemuir.txt"]
+--  weatherStations <- mapM (readFile . ("stations/"++)) ["armagh.txt", "heathrow.txt", "lowestoft.txt", "rossonwye.txt", "sheffield.txt", "nairn.txt", "eskdalemuir.txt"]
+  weatherStations <- mapM (readFile . ("stations/"++)) ["armagh.txt", "heathrow.txt", "lowestoft.txt", "rossonwye.txt", "sheffield.txt", "nairn.txt", "durham.txt"]
+
+  -- This should be 0.67
+  putStrLn $ show $ spearmans [56.0,75.0,45.0,71.0,62.0,64.0,58.0,80.0,76.0,61.0] [66.0,70.0,40.0,60.0,65.0,56.0,59.0,77.0,67.0,63.0]
 
   toFile def "test.png" $ do
     layout_title .= "Test Graph"
     layout_x_axis . laxis_title .= "Average Temperature (°C)"
     layout_y_axis . laxis_title .= "Rainfall (cm)"
-    setColors [opaque blue, opaque red, opaque cyan]
+    setColors [opaque blue, opaque red, opaque cyan, opaque darkred, opaque green, opaque dimgrey, opaque deeppink]
     mapM_ plotStation_Max $ map parseData weatherStations
